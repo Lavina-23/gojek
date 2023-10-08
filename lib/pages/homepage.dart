04d44b0b9c525301ac2987484299fef5d55bd5f9
Lavina-23 <lavinakohai3227@gmail.com>
2023-10-08 20:47:51 +0700

    slicing quick access

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -239,7 +239,57 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
               ),
-            )
+            ),
+            // Akses 
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Akses Cepat', style: bold18.copyWith(color: dark1),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 110,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Color(0xFFE8E8E8))
+                    ),
+                    child: Column(
+                      children: [
+                      ...['Pintu masuk motor, MNC Land', 'Pintu keluar motor, MNC Land'].map((text) =>
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Row(
+                            children: [Container(
+                              height: 24,
+                              width: 24,
+                              decoration: BoxDecoration(
+                                color: green1,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: SvgPicture.asset('assets/icons/goride.svg', height: 24,),
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                              Text(text, style: regular14.copyWith(color: dark2),),
+                              const SizedBox(
+                                width: 70,
+                              ),
+                              SvgPicture.asset('assets/icons/left.svg', color: dark1,)
+                            ],
+                          ),
+                        ),),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            
           ],
         ),
       ),
