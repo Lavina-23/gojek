36a918619979743ca59f2b08196dd2cd6f81fa62
Lavina-23 <lavinakohai3227@gmail.com>
2023-10-08 19:19:54 +0700

    slicing menu's icons

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -128,6 +128,42 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+            // Icons Menu
+            Padding(
+              padding: const EdgeInsets.only(top: 32, left: 27, right: 27, bottom: 19),
+              child: SizedBox(
+                height: 157,
+                child: GridView.count(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 8,
+                children: [
+                  ...menuIcons.map((icon) => Container(
+                    child: Column(
+                      children: [
+                        Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: icon.icon == 'goclub'? Colors.white : icon.color,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: SvgPicture.asset('assets/icons/${icon.icon}.svg',
+                            color: icon.icon == 'goclub'? icon.color : icon.icon == 'other'? dark2 : Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 9,
+                          ),
+                          Text(
+                            icon.title,
+                            style: regular12_5.copyWith(color: Color(0xFF4A4A4A)),
+                          )
+                      ],
+                    ),
+                  ))
+                ],),
+              ),
+            )
           ],
         ),
       ),
