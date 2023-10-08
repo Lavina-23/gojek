62269e9e21f963773ff487b218f8518e5e42e7d2
Lavina-23 <lavinakohai3227@gmail.com>
2023-10-08 08:11:11 +0700

    slicing search

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gojek/components/header.dart';
 import 'package:gojek/theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,37 +13,83 @@ class HomePage extends StatelessWidget {
         backgroundColor: green2,
         elevation: 0,
         toolbarHeight: 80,
-        title: 
-        Container(
-          padding: const EdgeInsets.all(5),
-          decoration: BoxDecoration(color: green1, borderRadius: BorderRadius.circular(30)),
-        child: Row(
+        title: const Header(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(100),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+              child: Row(
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFAFAFA),
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: dark3),
+                      ),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/icons/search.svg',
+                            color: dark1,
+                            width: 20,
+                            height: 20,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Cari layanan, makanan, & tujuan',
+                            style: regular14.copyWith(color: dark3),  
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-              child: Text(
-                'Beranda',
-                style: semibold14.copyWith(color: green1),
-                ),
-            ),
-            ...['Promo', 'Pesanan', 'Chat'].map((title) => Flexible(
-              fit: FlexFit.loose,
-              child: Padding(
-                padding : const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
-                child: Center(
-                  child: Text(
-                    title,
-                    style: semibold14,
+                  const SizedBox(
+                    width: 15,
                   ),
-                ),
+                  SizedBox(
+                    width: 35,
+                    height: 35,
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35/2),
+                          ),
+                          clipBehavior: Clip.hardEdge, 
+                          child: Image.asset('assets/images/avatar.png'),
+                        ),
+                        Positioned(
+                          right: 0,
+                          bottom: 0,
+                            child: Container(
+                              width: 16,
+                              height: 16,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(35/2),
+                              color: const Color(0xFFD1E7EE),
+                            ),
+                          clipBehavior: Clip.hardEdge,
+                          child: SvgPicture.asset(
+                            'assets/icons/goclub.svg',
+                            color: blue2,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ))
-          ],),
-        ), 
+            )
+          ],
+        ),
       ),
     );
   }
